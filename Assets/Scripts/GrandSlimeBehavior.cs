2414c3e25d790a65b71b18d86ab179b224f7074d
KhoaHN612 <khoahnvithuy@gmail.com>
2022-09-23 10:29:59 +0700

    23/9/2022

---
+++ Assets/Scripts/GrandSlimeBehavior.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandSlimeBehavior : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            body.velocity = new Vector2(body.velocity.x, 10);
+        }
+    }
+}
